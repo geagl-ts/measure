b43203b864358edb7d9248ae035eaec96580a436
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-04-28 11:58:59 -0500

    :avance: La navegacion del home ya se ve

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Login, About, Home, SignUp, AuthLoading } from "./containers";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const BottomNavigator = () => {
+    return (
+        <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="About" component={About} />
+        </Tab.Navigator>
+    );
+};
 
 const StackNavigator = () => {
     return (
@@ -13,8 +24,7 @@ const StackNavigator = () => {
             <Stack.Screen name="AuthLoading" component={AuthLoading} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="About" component={About} />
+            <Stack.Screen name="HomeNavigator" component={BottomNavigator} />
         </Stack.Navigator>
     );
 };
