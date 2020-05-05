559f339d380f9ad010b8d2bebe8533f411847fe5
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-05-05 15:55:17 -0500

    campo de telefono y de medidas, listos

--- src/containers/NuevoCliente.js
+++ src/containers/NuevoCliente.js
@@ -1,122 +1,158 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-import { FormContainer, Input, Button } from "../components";
+import { FormContainer, Button } from "../components";
 import { ScrollView } from "react-native-gesture-handler";
 
-const ContenedorDeInput = ({ children }) => (
-    <View style={{ ...styles.contenedorDeInput }}>{children}</View>
-);
-
-const InputPerzonalizado = ({ titulo, placeholder, width }) => {
-    const Titulo = () => {
-        if (titulo) {
-            return <TituloDelInput>{titulo}</TituloDelInput>;
-        } else {
-            return <></>;
-        }
-    };
-
-    return (
-        <ContenedorDeInput>
-            <Titulo />
-            <Input
-                placeholder={titulo || placeholder}
-                shadow={true}
-                borderRadius={30}
-                placeholderColor="#c4c4c4"
-                width={width}
-            />
-        </ContenedorDeInput>
-    );
-};
-
-const TituloDelInput = ({ children }) => (
-    <Text style={{ ...styles.tituloDelInput }}>{children}</Text>
-);
+import { Picker } from "@react-native-community/picker";
 
 const Botonera = ({ children }) => (
     <View style={{ ...styles.botoneraContainer }}>{children}</View>
 );
 
+const inputStyles = StyleSheet.create({
+    container: {},
+    text: {
+        color: "#2ba6ff",
+        fontSize: 19,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: 1,
+        marginLeft: 20,
+    },
+    input: {
+        color: "#2ba6ff",
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+        fontSize: 18,
+        fontWeight: "bold",
+        marginHorizontal: 20,
+        borderBottomColor: "#2ba6ff",
+        borderBottomWidth: 2,
+        backgroundColor: "#f7f7f7",
+    },
+    warning: { color: "coral", fontSize: 16 },
+});
+
+const Input = ({
+    titulo,
+    placeholder,
+    warning,
+    customStyles,
+    inputCustomStyles,
+    keyboardType,
+}) => {
+    let titulo_modificado = "";
+
+    if (titulo) {
+        let titulo_string = String(titulo);
+
+        const dos_puntos = titulo_string.charAt(titulo_string.length - 1);
+
+        if (dos_puntos === ":") {
+            titulo_modificado = titulo_string
+                .substr(0, titulo_string.length - 1)
+                .toLowerCase();
+        } else {
+            titulo_modificado = titulo_string.toLowerCase();
+        }
+    }
+
+    return (
+        <View style={{ ...inputStyles.container, ...customStyles }}>
+            {titulo ? (
+                <Text style={{ ...inputStyles.text }}>{titulo}</Text>
+            ) : (
+                <></>
+            )}
+            <TextInput
+                style={{ ...inputStyles.input, ...inputCustomStyles }}
+                placeholder={
+                    placeholder
+                        ? placeholder.toLowerCase()
+                        : titulo_modificado.length > 0
+                        ? titulo_modificado
+                        : titulo_modificado
+                }
+                placeholderTextColor="#aaa"
+                autoCapitalize="none"
+                selectionColor="#2ba6ff"
+                keyboardType={keyboardType}
+            />
+            {warning ? (
+                <Text
+                    style={{
+                        ...inputStyles.text,
+                        ...inputStyles.warning,
+                    }}
+                >
+                    Warning
+                </Text>
+            ) : (
+                <></>
+            )}
+        </View>
+    );
+};
+
+const viewHorizontal = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+});
+
+const ViewHorizontal = ({ children }) => {
+    return <View style={{ ...viewHorizontal.container }}>{children}</View>;
+};
+
 const NuevoCliente = () => {
+    const [pickerValue, setPickerValue] = React.useState({ valor: "java" });
+
     return (
         <FormContainer paddingNone={true}>
             <ScrollView style={{ ...styles.scroll }}>
-                <View
+                <Input
+                    titulo="Nombre del cliente:"
+                    customStyles={{ marginTop: 20 }}
+                />
+                <Input
+                    titulo="Telefono:"
+                    customStyles={{ marginTop: 20 }}
+                    keyboardType="numeric"
+                />
+                <Text style={{ ...inputStyles.text, marginTop: 20 }}>
+                    Medidas
+                </Text>
+                <ViewHorizontal>
+                    <Input
+                        placeholder="Altura"
+                        inputCustomStyles={{ width: 100, textAlign: "center" }}
+                        keyboardType="numeric"
+                    />
+                    <Input
+                        placeholder="Cintura"
+                        customStyles={{
+                            marginHorizontal: -20,
+                        }}
+                        inputCustomStyles={{ width: 100, textAlign: "center" }}
+                        keyboardType="numeric"
+                    />
+                </ViewHorizontal>
+                {/* <Picker
+                    selectedValue={pickerValue.valor}
                     style={{
-                        alignItems: "center",
-                        height: 500,
+                        height: 60,
+                        width: "90%",
+                        color: "#2ba6ff",
                     }}
+                    onValueChange={(itemValue, itemIndex) =>
+                        setPickerValue({ valor: itemValue })
+                    }
                 >
-                    <InputPerzonalizado titulo="Nombre de la persona" />
-                    <View
-                        style={{
-                            marginTop: 30,
-                        }}
-                    >
-                        <TituloDelInput>Medidas</TituloDelInput>
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                        }}
-                    >
-                        <View
-                            style={{
-                                marginHorizontal: 10,
-                            }}
-                        >
-                            <InputPerzonalizado
-                                placeholder="altura"
-                                width={130}
-                            />
-                        </View>
-                        <View
-                            style={{
-                                marginHorizontal: 10,
-                            }}
-                        >
-                            <InputPerzonalizado
-                                placeholder="cintura"
-                                width={130}
-                            />
-                        </View>
-                    </View>
-                    <View
-                        style={{
-                            marginTop: 30,
-                        }}
-                    >
-                        <TituloDelInput>Telefono</TituloDelInput>
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                        }}
-                    >
-                        <View
-                            style={{
-                                marginHorizontal: 10,
-                            }}
-                        >
-                            <InputPerzonalizado
-                                placeholder="numero"
-                                width={190}
-                            />
-                        </View>
-                        <View
-                            style={{
-                                marginHorizontal: 10,
-                            }}
-                        >
-                            <InputPerzonalizado
-                                placeholder="tipo"
-                                width={100}
-                            />
-                        </View>
-                    </View>
-                </View>
+                    <Picker.Item label="Java" value="java" />
+                    <Picker.Item label="JavaScript" value="js" />
+                </Picker> */}
             </ScrollView>
             <Botonera>
                 <Button
@@ -152,17 +188,5 @@ const styles = StyleSheet.create({
         bottom: 0,
         justifyContent: "center",
         alignItems: "center",
-    },
-    contenedorDeInput: {
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 15,
-    },
-    tituloDelInput: {
-        fontSize: 22,
-        fontWeight: "bold",
-        letterSpacing: 1,
-        color: "#2ba6ff",
     },
 });
