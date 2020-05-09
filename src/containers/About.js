f88688d1fcab9a86770f1743fbaf2986af971527
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-05-09 17:50:02 -0500

    acerca de, listo

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,6 +2,22 @@ import React from "react";
 import { View, Image, StyleSheet, AsyncStorage } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Link from "../containers/Link";
+import { Text } from "../components";
+
+//get user query
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+//query
+const GET_USER_DATA = gql`
+    query {
+        getUser {
+            userData {
+                email
+            }
+        }
+    }
+`;
 
 const styles = StyleSheet.create({
     container: {
@@ -39,42 +55,100 @@ const styles = StyleSheet.create({
 });
 
 const About = ({ navigation }) => {
+    //implement useQuery Hook
+    const { loading, data, refetch } = useQuery(GET_USER_DATA);
+
+    // //useEffect que se activa cuando regreso a la pantalla
+    // useEffect(() => {
+    //     const unsubscribe = navigation.addListener("focus", async () => {
+    //         refetch();
+    //     });
+
+    //     return unsubscribe;
+    // }, [navigation]);
+
     const onPressLink = async () => {
         await AsyncStorage.removeItem("Token");
         navigation.navigate("AuthLoading");
     };
 
-    return (
-        <LinearGradient
-            colors={["#2ba6ff", "#2bffed"]}
-            style={styles.container}
-        >
-            <View style={[styles.aboutContainer, styles.shadow]}>
-                <Image
-                    source={require("../../assets/signin.png")}
-                    style={styles.image}
-                />
-                <View
-                    style={{
-                        width: "100%",
-                        height: "60%",
-                        alignItems: "center",
-                        justifyContent: "flex-end",
-                    }}
-                >
-                    <Link
-                        fontWeight="bold"
-                        fontSize={20}
-                        color="#2ba6ff"
-                        onPress={onPressLink}
-                        uppercase={true}
-                        letterSpacing={1}
-                    >
-                        Cerrar sesion
-                    </Link>
+    if (loading) {
+        return (
+            <LinearGradient
+                colors={["#2ba6ff", "#2bffed"]}
+                style={styles.container}
+            >
+                <View style={[styles.aboutContainer, styles.shadow]}>
+                    <Image
+                        source={require("../../assets/signin.png")}
+                        style={styles.image}
+                    />
                 </View>
-            </View>
-        </LinearGradient>
+            </LinearGradient>
+        );
+    } else {
+        const { email } = data.getUser.userData;
+
+        return (
+            <LinearGradient
+                colors={["#2ba6ff", "#2bffed"]}
+                style={styles.container}
+            >
+                <View style={[styles.aboutContainer, styles.shadow]}>
+                    <Image
+                        source={require("../../assets/signin.png")}
+                        style={styles.image}
+                    />
+                    <Title label="Correo Electronico">{email}</Title>
+                    <View
+                        style={{
+                            width: "100%",
+                            height: "55%",
+                            alignItems: "center",
+                            justifyContent: "flex-end",
+                            marginBottom: 25,
+                        }}
+                    >
+                        <Link
+                            fontWeight="bold"
+                            fontSize={20}
+                            color="#2ba6ff"
+                            onPress={onPressLink}
+                            uppercase={true}
+                            letterSpacing={1}
+                        >
+                            Cerrar sesion
+                        </Link>
+                    </View>
+                </View>
+            </LinearGradient>
+        );
+    }
+};
+
+const titleStyles = StyleSheet.create({
+    container: {
+        width: "90%",
+    },
+    text: {
+        color: "#2ba6ff",
+        fontSize: 26,
+    },
+});
+
+const Title = ({ label, children }) => {
+    return (
+        <View style={{ ...styles.container }}>
+            <Text fontWeight="bold" otherStyles={{ ...titleStyles.text }}>
+                --- {label} ---
+            </Text>
+            <Text
+                fontWeight="medium"
+                otherStyles={{ ...titleStyles.text, fontSize: 24 }}
+            >
+                {children}
+            </Text>
+        </View>
     );
 };
 
