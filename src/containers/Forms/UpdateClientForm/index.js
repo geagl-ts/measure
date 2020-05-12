9cb13efedaa83e1238fc383ea3f3385a2a231b9a
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-05-12 10:01:41 -0500

    El updateClientForm esta comentado

--- src/containers/Forms/UpdateClientForm/index.js
+++ src/containers/Forms/UpdateClientForm/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
+//mis componentes
 import {
     ContenedorEstandar,
     Titulo,
@@ -8,23 +11,32 @@ import {
     Separador,
 } from "../../../components";
 
+//constantes
 import { COLOR_ESTANDAR, PLACEHOLDER_COLOR_ESTANDAR } from "./LocalVariables";
 
-import { useMutation } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
+//Mutations
 import updateClient from "./Graphql/updateClient";
 
+//mutacion
 const UPDATE_CLIENT = gql`
     ${updateClient}
 `;
 
+//main component
 const UpdateClientForm = ({ route, navigation }) => {
+    //variable para el nuevo nombre
     const [name, setName] = React.useState("");
 
+    //implementacion de la mutacion
     const [updateClient] = useMutation(UPDATE_CLIENT);
 
+    /*
+        obtiene al cliente en los parametros de la 
+        navegacion
+    */
     const { clientId } = route.params;
 
+    //se ejecuta cuento pulso el boton de cambiar
     const onSubmit = async () => {
         const {
             data: {
@@ -42,6 +54,10 @@ const UpdateClientForm = ({ route, navigation }) => {
         }
     };
 
+    /**
+     * cambia el valor del name con lo que
+     * pongo en el input
+     */
     const onChangeNameField = (value) => {
         setName(value);
     };
